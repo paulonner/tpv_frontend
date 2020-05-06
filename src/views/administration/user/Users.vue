19b5b483c93c04db377cfbb03b738a1cc468562a
Miquel-Jr <44075753+Miquel-Jr@users.noreply.github.com>
2020-05-06 12:41:39 -0500

    Maquetado CRUD - PERFILES

--- src/views/administration/user/Users.vue
+++ src/views/administration/user/Users.vue
@@ -1,9 +1,169 @@
 <template>
-  <h1>Aquí se mostrará la lista de usuario</h1>
+  <v-data-table
+    :headers="headers"
+    :items="desserts"
+    sort-by="profile"
+    class="elevation-1"
+    :search="search"
+  >
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>PERFILES</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Buscar"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="blue" dark class="mb-2" v-on="on">Nuevo Perfil</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.idprofile" label="Id Perfil"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.idmenu" label="Id Menu"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.nameprofile" label="Nombre Perfil"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:item.actions="{ item }">
+      <v-icon
+        small
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
-  name: 'Users'
+  name: 'Users',
+
+  data: () => ({
+    dialog: false,
+    search: '',
+    headers: [
+      { text: 'Id Perfil', align: 'start', sortable: false, value: 'idprofile' },
+      { text: 'Id Menu', value: 'idmenu' },
+      { text: 'Nombre Perfil', value: 'nameprofile' },
+      { text: 'Acciones', value: 'actions', sortable: false }
+    ],
+    desserts: [],
+    editedIndex: -1,
+    editedItem: {
+      idprofile: 0,
+      idmenu: 0,
+      nameprofile: ''
+    },
+    defaultItem: {
+      idprofile: 0,
+      idmenu: 0,
+      nameprofile: ''
+    }
+  }),
+
+  computed: {
+    formTitle () {
+      return this.editedIndex === -1 ? 'Nuevo Perfil' : 'Editar Perfil'
+    }
+  },
+
+  watch: {
+    dialog (val) {
+      val || this.close()
+    }
+  },
+
+  created () {
+    this.initialize()
+  },
+
+  methods: {
+    initialize () {
+      this.desserts = [
+        {
+          idprofile: 1,
+          idmenu: 25,
+          nameprofile: 'Josesito'
+        },
+        {
+          idprofile: 2,
+          idmenu: 9,
+          nameprofile: 'Damian'
+        },
+        {
+          idprofile: 3,
+          idmenu: 2,
+          nameprofile: 'MiquelJr'
+        }
+      ]
+    },
+
+    editItem (item) {
+      this.editedIndex = this.desserts.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem (item) {
+      const index = this.desserts.indexOf(item)
+      confirm('¿Está seguro que desea eliminar este perfil?') && this.desserts.splice(index, 1)
+    },
+
+    close () {
+      this.dialog = false
+      this.$nextTick(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      })
+    },
+
+    save () {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem)
+      } else {
+        this.desserts.push(this.editedItem)
+      }
+      this.close()
+    }
+  }
 }
 </script>
